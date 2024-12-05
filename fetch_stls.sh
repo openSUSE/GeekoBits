143b02b5d01a42d8945ff9efc19af8cc6691d6e6
Lubos Kocman <Lubos.Kocman@suse.com>
2024-12-05 23:54:16 +0100

    Remove debug echo

--- fetch_stls.sh
+++ fetch_stls.sh
@@ -18,7 +18,7 @@ counter=0 # let's prefix partnums with incremental number to avoid dupes
 for partnum in `grep "\.dat" model.ldr | awk '{ printf "%s ", $NF}' | tr --delete '\n' | tr --delete '\r'`; do
     ((counter++)) # let's start from 1
     echo "Copying $partnum into stl/$counter-$partnum.stl"
-    echo dat2stl --file ldraw/parts/$partnum --ldrawdir ./ldraw > stl/$counter-$partnum.stl
+    dat2stl --file ldraw/parts/$partnum --ldrawdir ./ldraw > stl/$counter-$partnum.stl
 done
 
 echo "Compressing stl.tar.xz"
